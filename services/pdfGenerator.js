2bfa0677058aa91e241810f3421a67877fb29952
everedinsonabogado <everedinson@gmail.com>
2026-06-21 21:08:23 -0500

    Update pdfGenerator.js

--- services/pdfGenerator.js
+++ services/pdfGenerator.js
@@ -1,1 +1,49 @@
-module.exports = {};
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+function generarPDF(informe){
+
+    const nombreArchivo = uuidv4() + ".pdf";
+
+    const rutaPDF = path.join(
+        __dirname,
+        "..",
+        "pdf",
+        nombreArchivo
+    );
+
+    const doc = new PDFDocument({
+        margin: 40
+    });
+
+    doc.pipe(fs.createWriteStream(rutaPDF));
+
+    doc.fontSize(22)
+       .text(
+          "INFORME DE ANALISIS DE EXPEDIENTE JUDICIAL",
+          { align:"center" }
+       );
+
+    doc.moveDown();
+
+    doc.fontSize(11)
+       .text(
+          "Ever Edinson Abogado - Especialista en Remates Judiciales"
+       );
+
+    doc.moveDown();
+
+    doc.fontSize(12)
+       .text(informe);
+
+    doc.end();
+
+    return nombreArchivo;
+
+}
+
+module.exports = {
+    generarPDF
+};
