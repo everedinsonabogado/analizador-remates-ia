2610084213c317292ca0ab4509e90995eb6cb2e5
everedinsonabogado <everedinson@gmail.com>
2026-06-21 21:06:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -56,32 +56,6 @@ ${texto}
 
 const informe = respuesta.output_text;
 
-const nombreArchivo = uuidv4() + ".pdf";
-
-const rutaPDF = path.join(__dirname, "pdf", nombreArchivo);
-
-const doc = new PDFDocument({
-    margin: 40
-});
-
-doc.pipe(fs.createWriteStream(rutaPDF));
-
-doc.fontSize(22).text("INFORME DE ANALISIS DE EXPEDIENTE JUDICIAL", {
-    align: "center"
-});
-
-doc.moveDown();
-
-doc.fontSize(11).text(
-    "Generado por Ever Edinson Abogado - Especialista en Remates Judiciales"
-);
-
-doc.moveDown();
-
-doc.fontSize(12).text(informe);
-
-doc.end();
-
 res.json({
 
     informe: informe,
